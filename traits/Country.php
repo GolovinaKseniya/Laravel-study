0281a9e9382cc98d86b2bf3daac334af701cd1fe
Kseniya Golovina <kseniya.golovina@devit-team.com>
2021-04-09 11:50:08 +0300

    Interfaces and Traits text material

---
+++ traits/Country.php
@@ -0,0 +1,16 @@
+
+class Country {
+    public $name, $age, $population;
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getPopulation() {
+        return $this->population;
+    }
+}
