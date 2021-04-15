94f2a4e9217687a795d16a7c0410e1937e86ae3b
GolovinaKseniya <55711138+GolovinaKseniya@users.noreply.github.com>
2021-04-15 23:17:46 +0300

    4 task

--- php-tasks/4.php
+++ php-tasks/4.php
@@ -1,20 +1,23 @@
 <?php
 
 $find_value = 't';
-
-$array = [['a', 3, 65, 'ft'], ['fd', 4, 615, 't']];
+$array = [['a', 3, 65, 'ft'], ['fd', 4, 615, 't4'], 't'];
 
 function findValue($value, $array)
 {
     foreach ($array as $item) {
         if (is_array($item)) {
-            findValue($value, $item);
+            $var = findValue($value, $item);
+            if (!$var) {
+                continue;
+            } else return true;
         } else {
-            if ($item == $value) {
-                var_dump($value);
+            if ($item === $value) {
+                return true;
             }
         }
     }
+    return false;
 }
 
 var_dump(findValue($find_value, $array));
