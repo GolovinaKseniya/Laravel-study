f47f28fe8dcc8829ca8e737b08d56327f7ed7977
GolovinaKseniya <55711138+GolovinaKseniya@users.noreply.github.com>
2021-04-17 21:18:27 +0300

    Task #10

---
+++ php-tasks/10.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Написать функцию которая будет генерировать уникальную строку
+ * */
+
+function generateUUID () {
+    return generateSymbols(8)."-".
+            generateSymbols(4)."-".
+            generateSymbols(4)."-".
+            generateSymbols(4)."-".
+            generateSymbols(12);
+}
+
+function generateSymbols($quantity) {
+    $res = "";
+    $symbols = "abcdef0123456789";
+
+
+    for($i = 0; $i < $quantity; $i++) {
+        $res .= $symbols[rand(0, strlen($symbols) - 1)];
+    }
+
+    return $res;
+}
+
+echo generateUUID();
